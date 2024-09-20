0230cec1b379f854c3b2046727440914729defa3
jeremy <clodeindustrie@users.noreply.github.com>
2024-09-20 11:23:32 +1000

    Update emacs to 30.0.60

--- flake.nix
+++ flake.nix
@@ -69,7 +69,7 @@
 
         emacs = (prev.emacs.override { srcRepo = true; withNativeCompilation = true; withXwidgets = false; withGTK3 = true; }).overrideAttrs (
           o: rec {
-            version = "30.0.50";
+            version = "30.0.60";
             src = emacs-src;
 
             buildInputs = o.buildInputs ++ [ prev.darwin.apple_sdk.frameworks.WebKit ];
